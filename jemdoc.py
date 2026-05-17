4d8c1aedb60a9ef452decfdf2723e294539d7d9f
Kevin Zagalo <kevin.zagalo@inria.fr>
2026-05-17 10:22:06 +0200

    back to top button

--- jemdoc.py
+++ jemdoc.py
@@ -261,7 +261,7 @@ def standardconf():
   </div>
   
   [lastupdated]
-  Page generated |, by a slightly modified version of <a href="https://github.com/mengzili/jemdoc-python3">jemdoc</a>.
+  Page generated |, by a slightly modified version of <a href="https://github.com/jem/jemdoc">jemdoc</a>.
 
   [sourcelink]
   (<a href="|" target="_blank">source</a>)
